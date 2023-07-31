d37b9411edc60455f680b5f85542dcd9b98424b1
minomad <93kymins@gmail.com>
2023-07-31 13:26:05 +0900

    feat: visitLike page 랜더링 작업

--- client/js/visitLike.js
+++ client/js/visitLike.js
@@ -1,11 +1,30 @@
-import { addClass, getNode, getNodes, keywordSwiper, removeClass, saveStorage } from '../lib/index.js';
-// const reviewForm = getNode('#reviewForm');
+import { getNode, getNodes, keywordSwiper, saveStorage, tiger } from '../lib/index.js';
+
+const swiperWrapper = getNode('.swiper-wrapper');
 const reviewTextField = getNode('#reviewTextField');
 const reviewSubmitButton = getNode('.reviewSubmitButton');
 const keywordButton = getNodes('.swiper-slide button');
 const count = getNode('.reviewTextFieldCount');
 
-function handleTextField(e) {
+async function fetchData() {
+  try {
+    const response = await tiger.get('http://localhost:3000/data');
+    if (response.ok) {
+      const data = await response.data;
+      // renderKeyword(swiperWrapper, data[0]);
+    }
+  } catch (error) {
+    console.error('에러', error);
+  }
+}
+
+fetchData();
+
+
+
+
+
+async function handleTextField(e) {
   e.preventDefault();
 
   const value = reviewTextField.value;
@@ -13,36 +32,27 @@ function handleTextField(e) {
 
   const textLength = value.length;
   count.textContent = textLength;
+
+  const url = 'http://localhost:3000/data';
+  const body = { review: value }; // POST 요청에 담을 데이터 객체
+
+  try {
+    const response = await tiger.post(url, body);
+    if (response.ok) {
+      console.log('POST 성공!');
+    } else {
+      console.error('POST 실패!');
+    }
+  } catch (error) {
+    console.error('에러', error);
+  }
 }
 
 function handleButton(e) {
   e.preventDefault();
-  const URL = './visitRecord.html';
-  window.location.href = URL;
+  window.location.href = './visitRecord.html';
 }
 
-let selectedKeyword = null;
-
-keywordButton.forEach((item) => {
-  item.addEventListener('click', () => {
-    const clickedKeyword = item.textContent.trim();
-    if (selectedKeyword === clickedKeyword) {
-      selectedKeyword = null;
-      removeClass(item, 'bg-lionPrimary');
-    } else {
-      selectedKeyword = clickedKeyword;
-      keywordButton.forEach((button) => {
-        if (button.textContent.trim() !== selectedKeyword) {
-          removeClass(button, 'bg-lionPrimary');
-        }
-      });
-      addClass(item, 'bg-lionPrimary');
-    }
-    saveStorage('keywords', selectedKeyword ? [selectedKeyword] : []);
-  });
-});
-
+keywordSwiper();
 reviewTextField.addEventListener('input', handleTextField);
 reviewSubmitButton.addEventListener('click', handleButton);
-
-keywordSwiper();
--- client/js/visitRecord.js
+++ client/js/visitRecord.js
@@ -1,47 +1,67 @@
-import { getNode, insertFirst, loadStorage } from '../lib/index.js';
+import { getNode, insertLast, tiger } from '../lib/index.js';
 
 const visitList = getNode('.visit-list');
 const writeButton = getNode('.write-review');
 
-async function renderReviewDOM() {
+async function fetchData() {
   try {
-    const text = await loadStorage('review');
-    const selectedKeyword = await loadStorage('keywords');
-
-    renderReview(visitList, {
-      text: text,
-      keyword1: selectedKeyword[0],
-    });
-
-    writeButton.style.display = 'none';
+    const response = await tiger.get('http://localhost:3000/data');
+    if (response.ok) {
+      const data = await response.data;
+      renderReview(visitList, data[0]);
+    }
   } catch (error) {
     console.error('에러', error);
   }
 }
 
-function createReview({ text, keyword1 }) {
+fetchData();
+
+function createReview({name, month, date, menu, price, image, review }) {
   const template = /* html */ `
-    <div class="leading-5">
-      <p class="line-clamp-2 h-[40px] w-[167px] text-ellipsis"> ${text}</p>
-      <div class="mt-1">
-        <button class="bg-liteGray rounded px-2 py-[2px]">${keyword1}</button>
-        <button class="bg-liteGray rounded px-2 py-[2px]">✨+2</button>
-      </div>
-    </div>
-    <img src="../assets/images/food/test.svg" alt="도넛" class="h-[72px] w-20" />
-    `;
+        <section>
+            <h2 class="font-semibold leading-6">${month} 방문</h2>
+            <div class="my-4 rounded-lg bg-white p-3">
+              <div class="flex justify-between gap-1 pb-1">
+                <span class="font-semibold">${name}</span>
+                <span class="pt-1 text-xs text-gray-400">${date}&middot;1번째 방문</span>
+                <button class="pl-2">
+                  <img src="../assets/icon/icon-heart.svg" alt="하트" />
+                </button>
+                <button>
+                  <img src="../assets/icon/icon-more.svg" alt="더보기" />
+                </button>
+              </div>
+              <div class="flex gap-4 text-xs text-lionContent">
+                <div class="leading-5">
+                  <p class="line-clamp-2 h-[40px] w-[167px] text-ellipsis">
+                  </p>
+                  <div class="mt-1">
+                    <button class="bg-liteGray rounded px-2 py-[2px]"></button>
+                    <button class="bg-liteGray rounded px-2 py-[2px]"></button>
+                  </div>
+                </div>
+                <img src="../assets/images/${image.src}" alt="${image.alt}" class="h-[72px] w-20" />
+              </div>
+              <p class="text-xs text-gray-400">${menu} &middot; ${price}원</p>
+              <button
+                class="write-review justify-center-center mt-5 inline-flex items-center rounded-lg bg-lionPrimary px-24 py-[4px] text-xs font-semibold leading-4 text-white hover:bg-lionSecondary"
+                onclick="location.href = './visitLike.html'">
+                <img src="../assets/icon/icon-pencil.svg" alt="연필" class="mr-[5px] w-5" />
+                리뷰쓰기
+              </button>
+            </div>
+          </section>`;
   return template;
 }
 
 function renderReview(target, data) {
-  const userCardTemplate = createReview(data);
-  insertFirst(target, userCardTemplate);
+  insertLast(visitList, createReview(data));
 }
 
-function handleWriteButton() {
-  const URL = './visitLike.html';
-  window.location.href = URL;
-}
+// function handleWriteButton() {
+//   window.location.href = './visitLike.html';
+// }
 
-window.addEventListener('DOMContentLoaded', renderReviewDOM);
-writeButton.addEventListener('click', handleWriteButton);
+// writeButton.addEventListener('click', handleWriteButton);
+
