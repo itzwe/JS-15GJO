fafd9f942d401319ab6a5717720fcf4024549855
yxxung <loveus011@naver.com>
2023-08-01 23:34:54 +0900

    chore: 코드 수정

--- client/lib/dom/index.js
+++ client/lib/dom/index.js
@@ -12,6 +12,5 @@ export * from './renderKeyword.js';
 export * from './getInputValue.js';
 export * from './clearContents.js';
 export * from './controlElement.js';
-export * from './renderReview.js';
 export * from './renderTheme.js';
 export * from './renderThemeReview.js'
