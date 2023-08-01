e5a7b93d5a31451aab44232e9180242c3d7849bb
itzwe <itzwewewe@naver.com>
2023-08-01 18:31:56 +0900

    markup : 8,1 마무리

--- client/js/mapMain.js
+++ client/js/mapMain.js
@@ -2,15 +2,17 @@ import {renderMap} from "../lib/index.js"
 import { getNode } from "../lib/index.js";
 import {imageChange} from "../lib/index.js"
 import { mapFixHandler } from "../lib/index.js";
-import { prevImage,nextImage } from "../lib/index.js";
+import {prevImage,nextImage  } from "../lib/index.js";
 
 // import {} from "../lib/utils/mapIconChange.js"
 
 // mapApi('.box', 100, 500, 37.511371, 127.098339); //header 지도
 // mapApi('.box2', 100, 200, 37.511371, 127.098339); //footer 지도
 
+// renderMap(37.559035,126.922513, 450, '.box'); 곱창
 renderMap(37.559035,126.922513, 450, '.box');
-renderMap(37.559035,126.922513, 250, '.box2');
+renderMap(37.559035,126.922513 , 250, '.box2');
+
 
 // 하트 버튼 누르면 이미지 변경 부분
 const toggleIcon = getNode('.toggle-icon')
@@ -27,3 +29,7 @@ const nextButton = getNode('.next');
 
 prevButton.addEventListener('click', prevImage);
 nextButton.addEventListener('click', nextImage);
+
+
+const backBtn = getNode('.mapBackBtn')
+backBtn.addEventListener('click', ()=> history.back())
--- client/lib/utils/mapImageModal.js
+++ client/lib/utils/mapImageModal.js
@@ -1,33 +1,26 @@
 import {getNode, getNodes} from "../dom/getNode.js"
 
 const images  = getNodes('.modal-image');
-
 const modal = getNode('.modal'); 
-
 const modalImage = getNode('.modal-i'); 
 const close = getNode('.close');
+
 let currentIndex = 0;
 
-// 모달에 이미지를 설정하는 함수.
 export function setModalImage(index) {
   if (index >= images.length || index < 0) return;
-
   const imageUrl = images[index].getAttribute('src');
   modalImage.setAttribute('src', imageUrl);
   currentIndex = index;
 }
-
 // 이전 이미지로 이동하는 함수.
 export function prevImage() {
   setModalImage(currentIndex - 1);
 }
-
 // 다음 이미지로 이동하는 함수.
 export function nextImage() {
   setModalImage(currentIndex + 1);
 }
-
-// 이미지 클릭 이벤트를 추가.
 images.forEach((image, index) => {
   image.addEventListener('click', (e) => {
     e.preventDefault()
@@ -35,13 +28,17 @@ images.forEach((image, index) => {
     modal.style.display = 'block';
   });
 });
-
-// 이전, 다음 버튼 클릭 이벤트를 추가.
-
-
-// x 버튼 클릭 시 모달 닫기.
 window.addEventListener('click', (e) => {
   if (e.target === close) {
     modal.style.display = 'none';
+  }
+});
+window.addEventListener("keydown", (e) => {
+  if (modal.style.display === "block") {
+    if (e.key === "ArrowRight") {
+      nextImage();
+    } else if (e.key === "ArrowLeft") {
+      prevImage();
+    }
   }
 });
